15573d97fb7f5708ea6bd93ecdcd315305e01298
majirou <majirou2018@gmail.com>
2019-11-29 00:59:46 +0900

    quiz sample, temporary logo

---
+++ components/LogoImage.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+  .text-center
+    img.img-thumbnail(src="@/assets/img/logo.jpg" alt="ロゴ画像")
+</template>
+
+<style lang="scss" scoped>
+img{
+  max-width: 540px;
+}
+</style>
